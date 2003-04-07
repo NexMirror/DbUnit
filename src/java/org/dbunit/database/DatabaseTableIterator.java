ec1bc6165575ddf62b1040429354779b40b998b4
Manuel Laflamme <mlaflamm@@users.sourceforge.net>
2003-04-07 02:20:10 +0000

    DatabaseDataSet improvements: * Faster access to metadata cache. * Keep original database table ordering. * Use a load on demand iterator implementation.

---
+++ src/java/org/dbunit/database/DatabaseTableIterator.java
@@ -0,0 +1,63 @@
+/*
+ *
+ * The DbUnit Database Testing Framework
+ * Copyright (C)2002, Manuel Laflamme
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+package org.dbunit.database;
+
+import org.dbunit.dataset.ITableIterator;
+import org.dbunit.dataset.DataSetException;
+import org.dbunit.dataset.ITableMetaData;
+import org.dbunit.dataset.ITable;
+
+/**
+ * @author Manuel Laflamme
+ * @since Apr 6, 2003
+ * @version $Revision$
+ */
+class DatabaseTableIterator implements ITableIterator
+{
+    private final String[] _tableNames;
+    private final DatabaseDataSet _dataSet;
+    private int _index = -1;
+
+    public DatabaseTableIterator(String[] tableNames, DatabaseDataSet dataSet)
+    {
+        _tableNames = tableNames;
+        _dataSet = dataSet;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    // ITableIterator interface
+
+    public boolean next() throws DataSetException
+    {
+        _index++;
+        return _index < _tableNames.length;
+    }
+
+    public ITableMetaData getTableMetaData() throws DataSetException
+    {
+        return _dataSet.getTableMetaData(_tableNames[_index]);
+    }
+
+    public ITable getTable() throws DataSetException
+    {
+        return _dataSet.getTable(_tableNames[_index]);
+    }
+}
