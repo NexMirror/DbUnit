5a1973e39f6609765a71e2b6b9777b044ecb252d
Jeff Jensen <jjensen@intertech.com>
2017-09-23 21:09:24 -0500

    PostgresSQLOidIT: reformat only

--- dbunit/src/test/java/org/dbunit/ext/postgresql/PostgresSQLOidIT.java
+++ dbunit/src/test/java/org/dbunit/ext/postgresql/PostgresSQLOidIT.java
@@ -15,51 +15,55 @@ import java.sql.Types;
 
 import static org.junit.Assert.assertArrayEquals;
 
-public class PostgresSQLOidIT extends TestCase {
+public class PostgresSQLOidIT extends TestCase
+{
     private IDatabaseConnection _connection;
 
-    private static final String xmlData = "<?xml version=\"1.0\"?>" +
+ // @formatter:off
+    private static final String xmlData ="<?xml version=\"1.0\"?>" +
             "<dataset>" +
             "<T2 DATA=\"[NULL]\" />" +
             "<T2 DATA=\"\\[text UTF-8](Anything)\" />" +
             "</dataset>";
+ // @formatter:on
 
     protected void setUp() throws Exception
     {
         super.setUp();
-        //Load active postgreSQL profile and connection from Maven pom.xml.
+        // Load active postgreSQL profile and connection from Maven pom.xml.
         _connection = DatabaseEnvironment.getInstance().getConnection();
     }
 
     protected void tearDown() throws Exception
     {
         super.tearDown();
-        if(_connection!=null)
+        if (_connection != null)
         {
             _connection.close();
             _connection = null;
         }
     }
 
-    public void xtestOidDataType() throws Exception {
+    public void xtestOidDataType() throws Exception
+    {
         final String testTable = "t2";
-        assertNotNull( "didn't get a connection", _connection );
+        assertNotNull("didn't get a connection", _connection);
         DatabaseConfig config = _connection.getConfig();
-        config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new PostgresqlDataTypeFactory());
+        config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY,
+                new PostgresqlDataTypeFactory());
         Statement stat = _connection.getConnection().createStatement();
-        //DELETE SQL OID tables
+        // DELETE SQL OID tables
         stat.execute("DROP TABLE IF EXISTS " + testTable + ";");
 
-        //Create SQL OID tables
+        // Create SQL OID tables
         stat.execute("CREATE TABLE " + testTable + "(DATA OID);");
         stat.close();
 
-        try{
-            ReplacementDataSet dataSet = new ReplacementDataSet(
-                    new FlatXmlDataSetBuilder().build(
-                            new InputSource(
-                                    new StringReader(xmlData))
-                    ));
+        try
+        {
+            ReplacementDataSet dataSet =
+                    new ReplacementDataSet(new FlatXmlDataSetBuilder()
+                            .build(new InputSource(new StringReader(xmlData))));
             dataSet.addReplacementObject("[NULL]", null);
             dataSet.setStrictReplacement(true);
 
@@ -68,20 +72,23 @@ public class PostgresSQLOidIT extends TestCase {
             ITableMetaData itmd = ids.getTableMetaData(testTable);
             Column[] cols = itmd.getColumns();
             ids = _connection.createDataSet();
-            for (Column col : cols) {
+            for (Column col : cols)
+            {
                 assertEquals(Types.BIGINT, col.getDataType().getSqlType());
                 assertEquals("oid", col.getSqlTypeName());
             }
-            //THE TEST -> hopefully with no exception!!!
+            // THE TEST -> hopefully with no exception!!!
             DatabaseOperation.CLEAN_INSERT.execute(_connection, dataSet);
             ids = _connection.createDataSet();
             ITable it = ids.getTable(testTable);
             assertNull(it.getValue(0, "DATA"));
-            assertArrayEquals("\\[text UTF-8](Anything)".getBytes(), (byte[])it.getValue(1, "DATA"));
+            assertArrayEquals("\\[text UTF-8](Anything)".getBytes(),
+                    (byte[]) it.getValue(1, "DATA"));
 
-        }catch(Exception e)
+        } catch (Exception e)
         {
-            assertEquals("DatabaseOperation.CLEAN_INSERT... no exception",""+e);
+            assertEquals("DatabaseOperation.CLEAN_INSERT... no exception",
+                    "" + e);
         }
     }
 }
