8ba317937b36181479474003b4e303423df4ed15
Jeff Jensen <jjensen@apache.org>
2019-02-08 14:13:16 -0600

    SortedTable: reformat only
    In prep for changes.

--- dbunit/src/main/java/org/dbunit/dataset/SortedTable.java
+++ dbunit/src/main/java/org/dbunit/dataset/SortedTable.java
@@ -34,14 +34,14 @@ import org.slf4j.LoggerFactory;
  * This is a ITable decorator that provide a sorted view of the decorated table.
  * This implementation does not keep a separate copy of the decorated table
  * data.
- * 
+ *
  * @author Manuel Laflamme
  * @author Last changed by: $Author$
- * @version $Revision$ $Date: 2009-05-01 02:56:07 -0500 (Fri, 01 May 2009)
- *          $
+ * @version $Revision$ $Date: 2009-05-01 02:56:07 -0500 (Fri, 01 May 2009) $
  * @since Feb 19, 2003
  */
-public class SortedTable extends AbstractTable {
+public class SortedTable extends AbstractTable
+{
 
     /**
      * Logger for this class
@@ -60,14 +60,16 @@ public class SortedTable extends AbstractTable {
 
     /**
      * Sort the decorated table by specified columns order.
-     * 
+     *
      * @param table
      *            decorated table
      * @param columns
      *            columns to be used for sorting
      * @throws DataSetException
      */
-    public SortedTable(ITable table, Column[] columns) throws DataSetException {
+    public SortedTable(final ITable table, final Column[] columns)
+            throws DataSetException
+    {
         _table = table;
         _columns = validateAndResolveColumns(columns);
         initialize();
@@ -75,15 +77,16 @@ public class SortedTable extends AbstractTable {
 
     /**
      * Sort the decorated table by specified columns order.
-     * 
+     *
      * @param table
      *            decorated table
      * @param columnNames
      *            names of columns to be used for sorting
      * @throws DataSetException
      */
-    public SortedTable(ITable table, String[] columnNames)
-            throws DataSetException {
+    public SortedTable(final ITable table, final String[] columnNames)
+            throws DataSetException
+    {
         _table = table;
         _columns = validateAndResolveColumns(columnNames);
         initialize();
@@ -92,7 +95,7 @@ public class SortedTable extends AbstractTable {
     /**
      * Sort the decorated table by specified metadata columns order. All
      * metadata columns will be used.
-     * 
+     *
      * @param table
      *            The decorated table
      * @param metaData
@@ -100,35 +103,38 @@ public class SortedTable extends AbstractTable {
      *            used for sorting the table
      * @throws DataSetException
      */
-    public SortedTable(ITable table, ITableMetaData metaData)
-            throws DataSetException {
+    public SortedTable(final ITable table, final ITableMetaData metaData)
+            throws DataSetException
+    {
         this(table, metaData.getColumns());
     }
 
     /**
      * Sort the decorated table by its own columns order which is defined by
      * {@link ITable#getTableMetaData()}. All table columns will be used.
-     * 
+     *
      * @param table
      *            The decorated table
      * @throws DataSetException
      */
-    public SortedTable(ITable table) throws DataSetException {
+    public SortedTable(final ITable table) throws DataSetException
+    {
         this(table, table.getTableMetaData());
     }
 
     /**
      * Verifies that all given columns really exist in the current table and
      * returns the physical {@link Column} objects from the table.
-     * 
+     *
      * @param columns
      * @return
      * @throws DataSetException
      */
-    private Column[] validateAndResolveColumns(Column[] columns)
-            throws DataSetException {
-        ITableMetaData tableMetaData = _table.getTableMetaData();
-        Column[] resultColumns =
+    private Column[] validateAndResolveColumns(final Column[] columns)
+            throws DataSetException
+    {
+        final ITableMetaData tableMetaData = _table.getTableMetaData();
+        final Column[] resultColumns =
                 Columns.findColumnsByName(columns, tableMetaData);
         return resultColumns;
     }
@@ -136,20 +142,22 @@ public class SortedTable extends AbstractTable {
     /**
      * Verifies that all given columns really exist in the current table and
      * returns the physical {@link Column} objects from the table.
-     * 
+     *
      * @param columnNames
      * @return
      * @throws DataSetException
      */
-    private Column[] validateAndResolveColumns(String[] columnNames)
-            throws DataSetException {
-        ITableMetaData tableMetaData = _table.getTableMetaData();
-        Column[] resultColumns =
+    private Column[] validateAndResolveColumns(final String[] columnNames)
+            throws DataSetException
+    {
+        final ITableMetaData tableMetaData = _table.getTableMetaData();
+        final Column[] resultColumns =
                 Columns.findColumnsByName(columnNames, tableMetaData);
         return resultColumns;
     }
 
-    private void initialize() {
+    private void initialize()
+    {
         logger.debug("initialize() - start");
 
         // The default comparator is the one that sorts by string - for
@@ -161,25 +169,32 @@ public class SortedTable extends AbstractTable {
     /**
      * @return The columns that are used for sorting the table
      */
-    public Column[] getSortColumns() {
+    public Column[] getSortColumns()
+    {
         return this._columns;
     }
 
-    private int getOriginalRowIndex(int row) throws DataSetException {
-        if (logger.isDebugEnabled()) {
-            logger.debug("getOriginalRowIndex(row={}) - start", Integer
-                    .toString(row));
+    private int getOriginalRowIndex(final int row) throws DataSetException
+    {
+        if (logger.isDebugEnabled())
+        {
+            logger.debug("getOriginalRowIndex(row={}) - start",
+                    Integer.toString(row));
         }
 
-        if (_indexes == null) {
-            Integer[] indexes = new Integer[getRowCount()];
-            for (int i = 0; i < indexes.length; i++) {
+        if (_indexes == null)
+        {
+            final Integer[] indexes = new Integer[getRowCount()];
+            for (int i = 0; i < indexes.length; i++)
+            {
                 indexes[i] = new Integer(i);
             }
 
-            try {
+            try
+            {
                 Arrays.sort(indexes, rowComparator);
-            } catch (DatabaseUnitRuntimeException e) {
+            } catch (final DatabaseUnitRuntimeException e)
+            {
                 throw (DataSetException) e.getCause();
             }
 
@@ -196,37 +211,44 @@ public class SortedTable extends AbstractTable {
      * implementation of the corresponding column DataType for comparing values
      * or not. Default value is <code>false</code> which means that the old
      * string comparison is used. <br>
-     * 
+     *
      * @param useComparable
      * @since 2.3.0
      */
-    public void setUseComparable(boolean useComparable) {
-        if (logger.isDebugEnabled()) {
-            logger.debug("setUseComparable(useComparable={}) - start", Boolean
-                    .valueOf(useComparable));
+    public void setUseComparable(final boolean useComparable)
+    {
+        if (logger.isDebugEnabled())
+        {
+            logger.debug("setUseComparable(useComparable={}) - start",
+                    Boolean.valueOf(useComparable));
         }
 
-        if (useComparable) {
+        if (useComparable)
+        {
             setRowComparator(new RowComparator(this._table, this._columns));
-        } else {
-            setRowComparator(new RowComparatorByString(this._table,
-                    this._columns));
+        } else
+        {
+            setRowComparator(
+                    new RowComparatorByString(this._table, this._columns));
         }
     }
 
     /**
      * Sets the comparator to be used for sorting the table rows.
-     * 
+     *
      * @param comparator
      *            that sorts the table rows
      * @since 2.4.2
      */
-    public void setRowComparator(Comparator comparator) {
-        if (logger.isDebugEnabled()) {
+    public void setRowComparator(final Comparator comparator)
+    {
+        if (logger.isDebugEnabled())
+        {
             logger.debug("setRowComparator(comparator={}) - start", comparator);
         }
 
-        if (_indexes != null) {
+        if (_indexes != null)
+        {
             // TODO this is an ugly design to avoid increasing the number of
             // constructors from 4 to 8. To be discussed how to implement it the
             // best way.
@@ -241,22 +263,30 @@ public class SortedTable extends AbstractTable {
     // //////////////////////////////////////////////////////////////////////////
     // ITable interface
 
-    public ITableMetaData getTableMetaData() {
+    @Override
+    public ITableMetaData getTableMetaData()
+    {
         logger.debug("getTableMetaData() - start");
 
         return _table.getTableMetaData();
     }
 
-    public int getRowCount() {
+    @Override
+    public int getRowCount()
+    {
         logger.debug("getRowCount() - start");
 
         return _table.getRowCount();
     }
 
-    public Object getValue(int row, String columnName) throws DataSetException {
-        if (logger.isDebugEnabled()) {
-            logger.debug("getValue(row={}, columnName={}) - start", Integer
-                    .toString(row), columnName);
+    @Override
+    public Object getValue(final int row, final String columnName)
+            throws DataSetException
+    {
+        if (logger.isDebugEnabled())
+        {
+            logger.debug("getValue(row={}, columnName={}) - start",
+                    Integer.toString(row), columnName);
         }
 
         assertValidRowIndex(row);
@@ -271,7 +301,8 @@ public class SortedTable extends AbstractTable {
      * Abstract class for sorting the table rows of a given table in a specific
      * order
      */
-    public static abstract class AbstractRowComparator implements Comparator {
+    public static abstract class AbstractRowComparator implements Comparator
+    {
         /**
          * Logger for this class
          */
@@ -286,44 +317,57 @@ public class SortedTable extends AbstractTable {
          * @param sortColumns
          *            The columns to be used for sorting in the given order
          */
-        public AbstractRowComparator(ITable table, Column[] sortColumns) {
+        public AbstractRowComparator(final ITable table,
+                final Column[] sortColumns)
+        {
             this._table = table;
             this._sortColumns = sortColumns;
         }
 
-        public int compare(Object o1, Object o2) {
+        @Override
+        public int compare(final Object o1, final Object o2)
+        {
             logger.debug("compare(o1={}, o2={}) - start", o1, o2);
 
-            Integer i1 = (Integer) o1;
-            Integer i2 = (Integer) o2;
+            final Integer i1 = (Integer) o1;
+            final Integer i2 = (Integer) o2;
 
-            try {
-                for (int i = 0; i < _sortColumns.length; i++) {
-                    String columnName = _sortColumns[i].getColumnName();
+            try
+            {
+                for (int i = 0; i < _sortColumns.length; i++)
+                {
+                    final String columnName = _sortColumns[i].getColumnName();
 
-                    Object value1 = _table.getValue(i1.intValue(), columnName);
-                    Object value2 = _table.getValue(i2.intValue(), columnName);
+                    final Object value1 =
+                            _table.getValue(i1.intValue(), columnName);
+                    final Object value2 =
+                            _table.getValue(i2.intValue(), columnName);
 
-                    if (value1 == null && value2 == null) {
+                    if (value1 == null && value2 == null)
+                    {
                         continue;
                     }
 
-                    if (value1 == null && value2 != null) {
+                    if (value1 == null && value2 != null)
+                    {
                         return -1;
                     }
 
-                    if (value1 != null && value2 == null) {
+                    if (value1 != null && value2 == null)
+                    {
                         return 1;
                     }
 
                     // Compare the two values with each other for sorting
-                    int result = compare(_sortColumns[i], value1, value2);
+                    final int result = compare(_sortColumns[i], value1, value2);
 
-                    if (result != 0) {
+                    if (result != 0)
+                    {
                         return result;
                     }
                 }
-            } catch (DataSetException e) {
+            } catch (final DataSetException e)
+            {
                 throw new DatabaseUnitRuntimeException(e);
             }
 
@@ -350,27 +394,31 @@ public class SortedTable extends AbstractTable {
      * order using the data type and the Comparable implementation the current
      * column has.
      */
-    protected static class RowComparator extends AbstractRowComparator {
+    protected static class RowComparator extends AbstractRowComparator
+    {
         /**
          * Logger for this class
          */
         private final Logger logger =
                 LoggerFactory.getLogger(RowComparator.class);
 
-        public RowComparator(ITable table, Column[] sortColumns) {
+        public RowComparator(final ITable table, final Column[] sortColumns)
+        {
             super(table, sortColumns);
         }
 
-        protected int compare(Column column, Object value1, Object value2)
-                throws TypeCastException {
-            if (logger.isDebugEnabled()) {
-                logger.debug(
-                        "compare(column={}, value1={}, value2={}) - start",
+        @Override
+        protected int compare(final Column column, final Object value1,
+                final Object value2) throws TypeCastException
+        {
+            if (logger.isDebugEnabled())
+            {
+                logger.debug("compare(column={}, value1={}, value2={}) - start",
                         new Object[] {column, value1, value2});
             }
 
-            DataType dataType = column.getDataType();
-            int result = dataType.compare(value1, value2);
+            final DataType dataType = column.getDataType();
+            final int result = dataType.compare(value1, value2);
             return result;
         }
 
@@ -380,29 +428,34 @@ public class SortedTable extends AbstractTable {
      * Compares the rows with each other in order to sort them in the correct
      * order using the string value of both values for the comparison.
      */
-    protected static class RowComparatorByString extends AbstractRowComparator {
+    protected static class RowComparatorByString extends AbstractRowComparator
+    {
         /**
          * Logger for this class
          */
         private final Logger logger =
                 LoggerFactory.getLogger(RowComparatorByString.class);
 
-        public RowComparatorByString(ITable table, Column[] sortColumns) {
+        public RowComparatorByString(final ITable table,
+                final Column[] sortColumns)
+        {
             super(table, sortColumns);
         }
 
-        protected int compare(Column column, Object value1, Object value2)
-                throws TypeCastException {
-            if (logger.isDebugEnabled()) {
-                logger.debug(
-                        "compare(column={}, value1={}, value2={}) - start",
+        @Override
+        protected int compare(final Column column, final Object value1,
+                final Object value2) throws TypeCastException
+        {
+            if (logger.isDebugEnabled())
+            {
+                logger.debug("compare(column={}, value1={}, value2={}) - start",
                         new Object[] {column, value1, value2});
             }
 
             // Default behavior since ever
-            String stringValue1 = DataType.asString(value1);
-            String stringValue2 = DataType.asString(value2);
-            int result = stringValue1.compareTo(stringValue2);
+            final String stringValue1 = DataType.asString(value1);
+            final String stringValue2 = DataType.asString(value2);
+            final int result = stringValue1.compareTo(stringValue2);
             return result;
         }
     }
@@ -410,8 +463,10 @@ public class SortedTable extends AbstractTable {
     /**
      * {@inheritDoc}
      */
-    public String toString() {
-        StringBuilder sb = new StringBuilder(2000);
+    @Override
+    public String toString()
+    {
+        final StringBuilder sb = new StringBuilder(2000);
 
         sb.append(getClass().getName()).append("[");
         sb.append("_columns=[").append(Arrays.toString(_columns)).append("], ");
